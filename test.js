214234be47ae417e45c98c14d7f638edd7841b55
system_D <faizanmorshid58@gmail.com>
2025-06-11 18:33:06 +0200

    Test file
    How does this works

---
+++ test.js
@@ -0,0 +1,8 @@
+const {Builder, Browser} = require('selenium-webdriver');
+
+(async function helloSelenium() {
+  let driver = await new Builder().forBrowser(Browser.CHROME).build();
+
+  await driver.get('https://selenium.dev');
+
+})();
